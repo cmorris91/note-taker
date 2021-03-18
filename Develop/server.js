8669598cf2d826b9796c9a9fa5c5df1465f3842e
Chelsey Morris <chelseymorris91@yahoo.com>
2021-03-17 20:06:43 -0700

    got routes to work except delete

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs')
-const notesData = JSON.parse(fs.readFileSync('./db/db.json'));
-// const uuid = require('uuid');
+const notesData = require("./db/db.json")
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -19,13 +21,23 @@ app.get('/api/notes', (req, res) => res.json(notesData));
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
+    newNote.id = uuidv4();
     notesData.push(newNote);
-    // newNote.id = uuid.v4()
     fs.writeFileSync('./db/db.json', JSON.stringify(notesData), (err) => {
         if (err) return console.log(err);
     res.json(notesData);
     })
 });
 
+// app.delete('/api/notes/:id', (req, res) => {
+    
+//     console.log(req.params.id)
+//     // notesData.delete(removedNote);
+//     // // console.log(notesData);
+//     // fs.writeFileSync('./db/db.json', JSON.stringify(notesData), (err) => {
+//     //     if (err) return console.log(err);
+//     // res.json(notesData);
+// })
+// })
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
